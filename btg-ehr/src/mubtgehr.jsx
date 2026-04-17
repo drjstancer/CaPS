10f057d730437bdf557e36eb7691852cd90cfd3e
drjstancer <drj.stancer@gmail.com>
2026-04-17 15:23:17 -0500

    Replace broken EHR file with deploy-safe version

--- btg-ehr/src/mubtgehr.jsx
+++ btg-ehr/src/mubtgehr.jsx
@@ -1,1 +1,11 @@
-REPLACEME
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>BtG EHR (Live)</h1>
+      <p>Your application is now successfully deployed.</p>
+      <p>Next step: we will plug your full case engine back in.</p>
+    </div>
+  );
+}
